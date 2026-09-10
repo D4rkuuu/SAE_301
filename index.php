f6fc13526b7c50fa7b36e20a0ab2af4d916a07ad
Emmanuel APPIAH <e.appiah2007@gmail.com>
2026-09-10 12:03:18 +0200

    Test Hello World

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<body>
+    <div class="container">
+        <h1>Hello World</h1>
+        <?php
+            $nom = "AlwaysData";
+            echo "<p>Test sur $nom.</p>";
+        ?>
+    </div>
+
+</body>
+</html>
